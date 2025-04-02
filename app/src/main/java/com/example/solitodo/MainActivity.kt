b1d8249ffbcb07e6e6eb6bd72288d137ebea4500
ChabbehAymen <devaymen15.5.3@gmail.com>
2025-04-02 13:01:17 +0000

    created module

--- app/src/main/java/com/example/solitodo/MainActivity.kt
+++ app/src/main/java/com/example/solitodo/MainActivity.kt
@@ -1,29 +1,53 @@
 package com.example.solitodo
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.runtime.key
-import com.example.solitodo.network.ApiService
-import com.example.solitodo.repository.TaskRepository
-import com.example.solitodo.useCase.CreateTaskUseCase
-import com.example.solitodo.useCase.DeleteTaskUseCase
-import com.example.solitodo.useCase.GetTasksUseCase
-import com.example.solitodo.useCase.UpdateTaskUseCase
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val repository: TaskRepository = TaskRepository(ApiService)
-        val getTaskUseCase = GetTasksUseCase(repository)
-        val deleteTaskUseCase = DeleteTaskUseCase(repository)
-        val createTaskUseCase = CreateTaskUseCase(repository)
-        val updateTaskUseCase = UpdateTaskUseCase(repository)
-        val vieModel = TaskVieModel(getTaskUseCase, deleteTaskUseCase, createTaskUseCase, updateTaskUseCase)
+        val vieModel = CountViewModel()
         enableEdgeToEdge()
         setContent {
-            TaskListScreen(vieModel)
+            Scaffold(
+                topBar = {
+
+                    TopAppBar(
+                        title = { Text("Solitodo") }
+                    )
+                },
+                content = {
+                    val count by vieModel.count
+                    Column (horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center, modifier = Modifier.fillMaxWidth().fillMaxHeight()){
+                        Button(onClick = { vieModel.increment() }) {
+                            Text("Increment")
+                        }
+                            Text("Count: $count")
+                        Button(onClick = { vieModel.decrement() }) {
+                            Text("Decrement")
+                        }
+                    }
+                }
+            )
         }
     }
 }
