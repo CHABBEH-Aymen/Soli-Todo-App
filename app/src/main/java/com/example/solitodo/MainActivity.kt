946799886f6c1cd31c1eb9cba136e235108b494b
ChabbehAymen <devaymen15.5.3@gmail.com>
2025-04-09 10:16:48 +0100

    completed

--- app/src/main/java/com/example/solitodo/MainActivity.kt
+++ app/src/main/java/com/example/solitodo/MainActivity.kt
@@ -3,27 +3,71 @@ package com.example.solitodo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.runtime.key
-import com.example.solitodo.network.ApiService
-import com.example.solitodo.repository.TaskRepository
-import com.example.solitodo.useCase.CreateTaskUseCase
-import com.example.solitodo.useCase.DeleteTaskUseCase
-import com.example.solitodo.useCase.GetTasksUseCase
-import com.example.solitodo.useCase.UpdateTaskUseCase
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.viewmodel.ui.theme.ViewModelTheme
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.viewmodel.viewModel.CounterViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val repository: TaskRepository = TaskRepository(ApiService)
-        val getTaskUseCase = GetTasksUseCase(repository)
-        val deleteTaskUseCase = DeleteTaskUseCase(repository)
-        val createTaskUseCase = CreateTaskUseCase(repository)
-        val updateTaskUseCase = UpdateTaskUseCase(repository)
-        val vieModel = TaskVieModel(getTaskUseCase, deleteTaskUseCase, createTaskUseCase, updateTaskUseCase)
-        enableEdgeToEdge()
         setContent {
-            TaskListScreen(vieModel)
+            ViewModelTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    CounterScreen()
+                }
+            }
         }
+    }
+}
+
+@Composable
+fun CounterScreen(counterViewModel: CounterViewModel = viewModel()) {
+    val count by counterViewModel.count.collectAsState()
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Compteur: $count", style = MaterialTheme.typography.headlineMedium)
+
+        Row(modifier = Modifier.padding(16.dp)) {
+            Button(onClick = { counterViewModel.decrement() }, modifier = Modifier.padding(8.dp)) {
+                Text(text = "-")
+            }
+
+            Button(onClick = { counterViewModel.increment() }, modifier = Modifier.padding(8.dp)) {
+                Text(text = "+")
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ViewModelTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ){
+            CounterScreen()
+        }
+
     }
 }
--- app/src/main/java/com/example/solitodo/dao/TaskDao.kt
+++
@@ -1,7 +0,0 @@
-package com.example.solitodo.dao
-
-data class TaskDao (
-    val id: Int? = null,
-    val title: String,
-    val description: String = "empty"
-)
